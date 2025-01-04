7281a8ce06069a084cac0c59fd16c380c604b083
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2025-01-04 21:37:17 +0600

    education section done

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import Title from "../Title/Title";
+import diuPic from "../../assets/Images/Daffodil.jpg";
 
 const Education = () => {
   return (
     <div>
       <Title title="Education"></Title>
+      <div className="md:flex items-center justify-center gap-20  ">
+        <div className="flex justify-center items-center">
+          <img className="w-60 " src={diuPic} alt="" />
+        </div>
+        <div className="flex flex-col justify-start items-center md:items-start gap-4">
+          <h1 className="text-navy font-bold text-xl md:text-2xl lg:text-4xl">
+            Daffodil International University
+          </h1>
+          <h2 className="text-bold font-semibold text-center md:text-left md:text-lg lg:text-2xl">
+            Bachelor of Science & Engineering
+          </h2>
+          <h2 className="text-bold font-semibold text-center md:text-left md:text-lg lg:text-2xl">
+            Dept of Computer Science And Engineering
+          </h2>
+          <p className="text-navy font-medium text-lg">Jan 2022 - Present</p>
+          <p className="text-navy font-medium">CGPA : 3.78</p>
+        </div>
+      </div>
     </div>
   );
 };
